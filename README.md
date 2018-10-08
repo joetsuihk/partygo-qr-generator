3e95b59a0430a05f5750b7db7eb7b1c34036ce43
joetsuihk <joe@documentonready.com>
2018-10-08 19:05:07 +0800

    init import

---
+++ README.md
@@ -0,0 +1,10 @@
+# Install 
+
+1. install pip https://pip.pypa.io/en/stable/installing/
+1. `sudo pip install pyqrcode`
+1. `sudo pip install Pillow`
+
+# Run
+1. copy tab separated values tp passcode.tsv
+2. `python main.py`
+3. you got the png QRcodes in export/
---
+++ main.py
@@ -0,0 +1,19 @@
+import pyqrcode
+from PIL import Image
+import csv
+
+with open("passcode.tsv") as fd:
+    rd = csv.reader(fd, delimiter="\t", quotechar='"')
+    for row in rd:
+        # print row
+        passcode = pyqrcode.QRCode(row[1], error = 'H')
+        passcode.png('test.png',scale=14)
+        im = Image.open('test.png')
+        im = im.convert("RGBA")
+        logo = Image.open('logo.png')
+        box = (155,155,255,255)
+        im.crop(box)
+        region = logo
+        region = region.resize((box[2] - box[0], box[3] - box[1]))
+        im.paste(region,box)
+        im.save('export/'+row[0]+'.png', "PNG")
